898d67c70bacc8d2d697036c279e6630a44959b8
mccreemainwoody <metz.clement@gmail.com>
2024-04-19 20:49:45 +0200

    Updating test files to make them dynamic when dealing with ressource ip resolution

--- tests/unit/test_demo_sqs_lambda_dynamodb.py
+++ tests/unit/test_demo_sqs_lambda_dynamodb.py
@@ -1,50 +1,69 @@
-import aws_cdk as cdk
 import aws_cdk.assertions as assertions
+import pytest
 
-from cdk.stacks.demo_sqs_lambda_dynamodb import SqsLambdaDynamoDB
+from aws_cdk import (
+    App,
+    aws_sqs as sqs,
+    aws_dynamodb as dynamodb,
+)
+
+from cdk_sqs_lambda_dynamodb.stacks import SqsLambdaDynamoDB
 
 
-def test_sqs_queue_has_dead_letter_queue():
-    app = cdk.App()
-    stack = SqsLambdaDynamoDB(app, "SqsLambdaDynamoDB")
-    template = assertions.Template.from_stack(stack)
+@pytest.fixture
+def stack():
+    return SqsLambdaDynamoDB(App(), "SqsLambdaDynamoDB")
 
+
+@pytest.fixture
+def template(stack) -> assertions.Template:
+    return assertions.Template.from_stack(stack)
+
+
+@pytest.fixture
+def queue(stack) -> sqs.Queue:
+    return stack.node.find_child("Queue")
+
+
+@pytest.fixture
+def dead_letter_queue(stack) -> sqs.Queue:
+    return stack.node.find_child("QueueDeadLetterQueue")
+
+
+@pytest.fixture
+def table(stack) -> dynamodb.Table:
+    return stack.node.find_child("Table")
+
+
+def test_sqs_queue_has_dead_letter_queue(stack, template, dead_letter_queue):
     template.has_resource_properties(
         "AWS::SQS::Queue",
         {
             "FifoQueue": True,
             "RedrivePolicy": {
-                "deadLetterTargetArn": {
-                    "Fn::GetAtt": ["QueueDeadLetterQueue16C1D65E", "Arn"]
-                },
+                "deadLetterTargetArn": stack.resolve(
+                    dead_letter_queue.queue_arn
+                ),
                 "maxReceiveCount": 10,
             },
         },
     )
 
 
-def test_lambda_function_has_correct_environment_variable():
-    app = cdk.App()
-    stack = SqsLambdaDynamoDB(app, "SqsLambdaDynamoDB")
-    template = assertions.Template.from_stack(stack)
-
+def test_lambda_function_has_correct_env_variable(stack, template, table):
     template.has_resource_properties(
         "AWS::Lambda::Function",
         {
             "Environment": {
                 "Variables": {
-                    "TABLE_NAME": {"Ref": "TableCD117FA1"},
+                    "TABLE_NAME": stack.resolve(table.table_name),
                 },
             },
         },
     )
 
 
-def test_dynamodb_table_has_correct_schema():
-    app = cdk.App()
-    stack = SqsLambdaDynamoDB(app, "SqsLambdaDynamoDB")
-    template = assertions.Template.from_stack(stack)
-
+def test_dynamodb_table_has_correct_schema(template):
     template.has_resource_properties(
         "AWS::DynamoDB::Table",
         {
@@ -60,14 +79,10 @@ def test_dynamodb_table_has_correct_schema():
     )
 
 
-def test_lambda_function_is_subscribed_to_sqs_queue():
-    app = cdk.App()
-    stack = SqsLambdaDynamoDB(app, "SqsLambdaDynamoDB")
-    template = assertions.Template.from_stack(stack)
-
+def test_lambda_function_is_subscribed_to_sqs_queue(stack, template, queue):
     template.has_resource_properties(
         "AWS::Lambda::EventSourceMapping",
         {
-            "EventSourceArn": {"Fn::GetAtt": ["Queue4A7E3555", "Arn"]},
+            "EventSourceArn": stack.resolve(queue.queue_arn)
         },
     )
--- tests/unit/test_src_stack.py
+++ tests/unit/test_src_stack.py
@@ -1,21 +1,22 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
-from cdk.stacks.src_stack import SrcStack
+import pytest
+
+from cdk_sqs_lambda_dynamodb.stacks import SrcStack
 
 
-def test_sqs_queue_created():
+@pytest.fixture
+def template():
     app = core.App()
     stack = SrcStack(app, "src")
-    template = assertions.Template.from_stack(stack)
+    return assertions.Template.from_stack(stack)
 
+
+def test_sqs_queue_created(template):
     template.has_resource_properties("AWS::SQS::Queue", {
         "VisibilityTimeout": 300
     })
 
 
-def test_sns_topic_created():
-    app = core.App()
-    stack = SrcStack(app, "src")
-    template = assertions.Template.from_stack(stack)
-
+def test_sns_topic_created(template):
     template.resource_count_is("AWS::SNS::Topic", 1)
